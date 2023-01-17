a07e4358e67d0b0fd4c61f0957095550cb5c4034
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-01-18 06:17:22 +0700

    Add html classname

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 const RootLayout = ({ children }) => (
-  <html lang="en">
+  <html lang="en" className="bg-primary-black h-full">
     <head>
       <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
     </head>
     <body>{children}</body>
   </html>
